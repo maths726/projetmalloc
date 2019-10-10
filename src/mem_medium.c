895c147f3f15ab51d9f75c6c7ec4bd8450ae57b9
William Goulois <william.goulois@grenoble-inp.org>
2019-10-10 18:21:28 +0200

    Ajout du mem_medium (non testé ni fini)

--- src/mem_medium.c
+++ src/mem_medium.c
@@ -9,31 +9,53 @@
 #include "mem.h"
 #include "mem_internals.h"
 
-unsigned int puiss2(unsigned long size) {
-    unsigned int p=0;
-    size = size -1; // allocation start in 0
-    while(size) {  // get the largest bit
-	p++;
-	size >>= 1;
+unsigned int puiss2(unsigned long size)
+{
+    unsigned int p = 0;
+    size = size - 1; // allocation start in 0
+    while (size)
+    { // get the largest bit
+        p++;
+        size >>= 1;
     }
     if (size > (1 << p))
-	p++;
+        p++;
     return p;
 }
 
+void *emalloc_medium_aux(unsigned long size, int *counter)
+{
+    unsigned long size_puiss2 = puiss2(size);
+    unsigned long *list_free_blocks = arena.TZL[size_puiss2];
+    if (list_free_blocks > (FIRST_ALLOC_MEDIUM_EXPOSANT + arena.medium_next_exponant))
+    {
+        mem_realloc_medium();
+    }
+    if (*list_free_blocks == NULL)
+    {
+        *counter++;
+        return emalloc_medium_aux(size * 2, &counter);
+    }
+    for (unsigned int i = 0; i < *counter; i++)
+    {
+        unsigned long *middle_free_block = *list_free_blocks + size / 2;
+        arena.TZL[size_puiss2 - i] = middle_free_block;
+    }
+
+    return (void *)0;
+}
 
 void *
 emalloc_medium(unsigned long size)
 {
     assert(size < LARGEALLOC);
     assert(size > SMALLALLOC);
-    /* ecrire votre code ici */
-    return (void *) 0;
+    int *counter;
+    *counter = 0;
+    return emalloc_medium_aux(size, counter);
 }
 
-
-void efree_medium(Alloc a) {
-    /* ecrire votre code ici */
+void efree_medium(Alloc a)
+{
+    void *adresse_buddy = a.ptr ^ a.size;
 }
-
-
